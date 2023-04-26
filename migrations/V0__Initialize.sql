358d07db9b88e73a11e9cc3d6cfe18863228f67c
pravez <pbreton@betomorrow.com>
2023-04-26 09:39:32 +0200

    Change triAmendement key length

--- migrations/V0__Initialize.sql
+++ migrations/V0__Initialize.sql
@@ -2,7 +2,7 @@ CREATE TABLE Amendments
 (
     uid                VARCHAR(256) PRIMARY KEY,
     examenRef          VARCHAR(256),
-    triAmendement      NVARCHAR(256),
+    triAmendement      NVARCHAR(512),
     texteLegislatifRef VARCHAR(256),
     dateDepot          DATETIME,
     datePublication    DATETIME,
